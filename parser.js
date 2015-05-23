79579afaa82e742bac732fb3455d4dd5d3f76053
= <gruber.asher@post.idc.ac.il>
2015-05-23 15:44:40 +0300

    request integration backup.

--- parser.js
+++ parser.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var request = require('request');
+var fs = require("fs");
+
 var app = express();
 
 var server = app.listen(3000, function () {
@@ -6,4 +9,16 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
 
     console.log('app listening at http://%s:%s', host, port);
+
+    parseSites();
 });
+
+function parseSites() {
+    var sites = ['http://www.cnn.com', 'http://www.ynet.co.il', 'http://www.idc.ac.il', 'http://www.haaretz.co.il'];
+
+    sites.forEach(function(siteURL) {
+        request(siteURL, function (error, response, body) {
+            console.log(body);
+        });
+    });
+}
