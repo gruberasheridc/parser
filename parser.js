a81c4e38dd21e39e1701877144c36f8d476fdb1d
= <gruber.asher@post.idc.ac.il>
2015-05-23 23:28:55 +0300

    request integration backup.

--- parser.js
+++ parser.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var request = require('request');
+var cheerio = require('cheerio');
+var validator = require('validator');
 var fs = require("fs");
+
 
 var app = express();
 
@@ -14,11 +17,32 @@ var server = app.listen(3000, function () {
 });
 
 function parseSites() {
-    var sites = ['http://www.cnn.com', 'http://www.ynet.co.il', 'http://www.idc.ac.il', 'http://www.haaretz.co.il'];
+    var sites = ['http://www.cnn.com', 'http://www.msn.com', 'https://www.yahoo.com', 'http://espn.go.com', 'http://www.imdb.com'];
 
     sites.forEach(function(siteURL) {
         request(siteURL, function (error, response, body) {
-            console.log(body);
+            if (!error) {
+                var $ = cheerio.load(body); // load the page into Cheerio.
+                var bodyText = $("body").text();
+
+                // Clear redundant whitespace from the sites text.
+                var content = bodyText.replace(/\s+/g, " ").toLowerCase();
+
+                // Collect links from the site.
+                var links = $("a"); // get all hyperlinks
+                $(links).each(function(i, link) {
+                    var url = $(link).attr("href");
+                    console.log(url);
+                    url = url.replace("/url?q=", "").split("&")[0];
+                    console.log(url);
+                    console.log(validator.isURL(url));
+                    console.log(validator.isURL(siteURL + url));
+                });
+
+                console.log(content);
+            } else {
+                console.log("Error while requesting site: " + siteURL + "." + " Error: " + error);
+            }
         });
     });
-}
+};
